1a071b1fd7d286de06453ba3c50f9a28cf937621
AndresGarciaLv <monsterreset38@gmail.com>
2025-03-22 21:18:40 -0500

    Fix: Contraseña opcional al editar un usuario

--- Bacheton.Application/User/Commands/Edit/EditUserCommand.cs
+++ Bacheton.Application/User/Commands/Edit/EditUserCommand.cs
@@ -3,4 +3,4 @@ using MediatR;
 
 namespace Bacheton.Application.User.Commands.Edit;
 
-public record EditUserCommand(Guid Id, string Name, string Email, string Password, Guid RoleId) : IRequest<ErrorOr<Updated>>;
+public record EditUserCommand(Guid Id, string Name, string Email, string? Password, Guid RoleId) : IRequest<ErrorOr<Updated>>;
